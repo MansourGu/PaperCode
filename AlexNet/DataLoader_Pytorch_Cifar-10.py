1e64d126ca9c62f98acde7e98cd303c2c6731a5b
MansourG <2364564092@qq.com>
2022-10-21 23:42:22 +0800

    添加了测试集的自定义数据集，以及解决了使用To_Tensor读取Numpy数组时出现的问题

--- AlexNet/DataLoader_Pytorch_Cifar-10.py
+++ AlexNet/DataLoader_Pytorch_Cifar-10.py
@@ -1,6 +1,9 @@
 import numpy as np
+import torch
 from torch.utils.data import DataLoader, Dataset
-file_path = 'E:/Code/DataSet/cifar-10-batches-py/data_batch_{}'
+from torchvision import transforms
+Train_file_path = 'E:/Code/DataSet/cifar-10-batches-py/data_batch_{}'
+Test_file_path = 'E:/Code/DataSet/cifar-10-batches-py/test_batch'
 def unpickle(file):
     import pickle
     with open(file, 'rb') as fo:
@@ -14,7 +17,7 @@ def Load_Cifar_10_Batch(batch_path):
     batch_data = batch_data.reshape(10000, 3, 32, 32)
     return np.array(batch_data), np.array(batch_label)
 
-def Load_Cifar_10_Dataset(file_path):
+def Load_Cifar_10_Train_Dataset(file_path):
     Data_Set, Data_Label = Load_Cifar_10_Batch(file_path.format(1))
     for i in range(2, 6):
         batch_path = file_path.format(i)
@@ -23,14 +26,23 @@ def Load_Cifar_10_Dataset(file_path):
         Data_Label = np.hstack((Data_Label, batch_label))
     return Data_Set, Data_Label
 
+def Load_Cifar_10_Test_Dataset(file_path):
+    Dataset, Data_Label = Load_Cifar_10_Batch(file_path)
+    return Dataset, Data_Label
+
 class Cifar10_DataSet(Dataset):
-    def __init__(self, file_path, transform=None):
-        self.DataSet, self.Data_Label = Load_Cifar_10_Dataset(file_path)
+    def __init__(self, file_path, train=True, transform=None):
+        self.train = train
+        if self.train == True:
+            self.DataSet, self.Data_Label = Load_Cifar_10_Train_Dataset(file_path)
+        else:
+            self.DataSet, self.Data_Label = Load_Cifar_10_Test_Dataset(file_path)
         self.transform = transform
 
     def __getitem__(self, item):
         img = self.DataSet[item]
         label = self.Data_Label[item]
+        img = torch.Tensor(img)
         if self.transform is not None:
             img = self.transform(img)
         return img, label
